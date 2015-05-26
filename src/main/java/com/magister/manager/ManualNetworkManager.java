fd832f76994b24d51dbbfb83c11f31ff8b1108ee
Vadim Kirilchuk <vadim.kirilchuk@macys.com>
2015-05-26 01:09:32 -0400

    Manual network manager just can recognize the same corner cases as automatic manager

--- src/main/java/com/magister/manager/ManualNetworkManager.java
+++ src/main/java/com/magister/manager/ManualNetworkManager.java
@@ -1,14 +1,23 @@
 package com.magister.manager;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.magister.db.domain.Mote;
 import com.magister.db.domain.Network;
+import com.magister.db.domain.Network.Status;
+import com.magister.db.repository.NetworkRepository;
 
 @Service
 public class ManualNetworkManager {
 
 //    private static final Logger LOG = LoggerFactory.getLogger(ManualNetworkManager.class);
+
+    @Autowired
+    private NetworkRepository networkRepository;
 
     @Transactional
     public void reorganizeTopology(Network network) {
@@ -18,6 +27,36 @@ public class ManualNetworkManager {
         // when D dies
         // G || A <-> C <-> B <-> A
         // need to find some path towards Gateway
+        List<Mote> motes = network.getMotes();
+
+        if (motes.size() == 0) {
+            network.setStatus(Status.NO_LIVE_MOTES);
+            networkRepository.save(network);
+            return;
+        }
+
+        Mote gateway = NetworkManager.findLiveGateway(network);
+        if (gateway == null) {
+            network.setStatus(Status.NO_LIVE_GATEWAYS);
+            networkRepository.save(network);
+            return;
+        }
+
+        // pick any gateway and create links between all nodes to gateway
+//        Topology topology = network.getTopology();
+//        List<MoteLink> links = topology.getLinks();
+//        links.clear();
+//        for (Mote mote : motes) {
+//            if (!mote.isGateway()) {
+//                MoteLink link = new MoteLink();
+//                link.setSource(mote);
+//                link.setTarget(gateway);
+//                links.add(link);
+//            }
+//        }
+
+        network.setStatus(Status.WORKING);
+        networkRepository.save(network);
     }
 
 }
